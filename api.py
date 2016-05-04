2d5ee0491db2d975740cf06c83bdfc62d6b93b4d
Boxuan Zhang <boxuan.zhang@drchrono.com>
2016-05-03 21:36:56 -0700

    finish basic game logic

---
+++ api.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import re
+import logging
+import endpoints
+from protorpc import remote, messages
+from google.appengine.api import memcache
+from google.appengine.api import taskqueue
+
+from models import User, Game, Score
+from models import StringMessage, NewGameForm, GameForm, MakeMoveForm,\
+    ScoreFroms
+from util import get_by_urlsafe
+
+USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
+                                           email=messages.StringField(2))
+NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
+GET_GAME_REQUEST = endpoints.ResourceContainer(urlsafe_game_key=messages.StringField(1),)
+MAKE_MOVE_REQUEST = endpoints.ResourceContainer(
+    MakeMoveForm,
+    urlsafe_game_key=messages.StringField(1),)
+
+@endpoints.api(name='hangman', version='v1')
+class HangmanApi(remote.Service):
+    """Game API"""
+
+    # Create a new user
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=StringMessage,
+                      path='user',
+                      name='create_user',
+                      http_method='Post')
+    def create_user(self, request):
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                'A user with that name already exists!')
+        user = User(name=request.user_name, email=request.email)
+        user.put()
+        return StringMessage(
+            message='User {} created!'.format(request.user_name))
+
+    # Start a new game
+    @endpoints.method(request_message=NEW_GAME_REQUEST,
+               response_message=GameForm,
+               path='game',
+               name='new_game',
+               http_method='POST')
+    def new_game(self, request):
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.FileNotFoundException(
+                'A user with that name does not exist')
+        game = Game.new_game(user.key, request.word)
+
+        return game.to_form('Good luck playing Hangman')
+
+    # Get a game status
+    @endpoints.method(request_message=GET_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='get_game',
+                      http_method='GET')
+    def get_game(self, request):
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game:
+            return game.to_form('Time to make a Guess')
+        else:
+            raise endpoints.FileNotFoundException('Game not found!')
+
+
+    # Make a move
+    @endpoints.method(request_message=MAKE_MOVE_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='make_move',
+                      http_method='PUT')
+    def make_move(self, request):
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if not game:
+            raise endpoints.FileNotFoundException('Game not found!')
+
+        if game.game_over:
+            return game.to_form('Game already over!')
+
+        guess = request.guess.upper()
+        if len(guess) != 1:
+            raise endpoints.BadRequestException('Guess can only be one character')
+
+        indexes= [m.start() for m in re.finditer(guess, game.target)]
+        if len(indexes) > 0:
+            currentResultList = list(game.current_result)
+            for idx in indexes:
+                currentResultList[idx] = guess
+            game.current_result = "".join(currentResultList)
+            game.put()
+            if game.current_result == game.target:
+                game.end_game(True)
+                return game.to_form('You win!')
+            else:
+                return game.to_form('You get a correct character')
+        else:
+            game.attempts += 1
+            game.put()
+            if game.attempts >= game.max_attempts:
+                game.end_game(False)
+                return game.to_form("Game over! The correct word is" + game.target)
+            else:
+                return game.to_form("Your guess is wrong!")
+
+api = endpoints.api_server([HangmanApi])
+
---
+++ models.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import random
+from datetime import date
+from protorpc import messages
+from google.appengine.ext import ndb
+
+# Game Models
+class User(ndb.Model):
+    """User Profile"""
+    name = ndb.StringProperty(required = True)
+    email = ndb.StringProperty()
+
+
+class Game(ndb.Model):
+    """Game Object"""
+    target = ndb.StringProperty(required = True)
+    current_result = ndb.StringProperty(required = True)
+    attempts = ndb.IntegerProperty(required = True)
+    max_attempts = ndb.IntegerProperty(required = True, default = 6)
+    game_over = ndb.BooleanProperty(required = True, default = False)
+    user = ndb.KeyProperty(required = True, kind = 'User')
+
+    @classmethod
+    def new_game(cls, user, word):
+        current_result = "".join(["_" for i in range(0, len(word))])
+        game = Game(user=user,
+                    target=word.upper(),
+                    current_result=current_result,
+                    attempts=0,
+                    max_attempts=6,
+                    game_over=False)
+        game.put()
+        return game
+
+    def end_game(self, won=False):
+        self.game_over = True
+        self.put()
+
+        score = Score(user=self.user,
+                      date=date.today(),
+                      won=won,
+                      score=self.max_attempts - self.attempts)
+        score.put()
+
+    def to_form(self, message):
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
+        form.attempts = self.attempts
+        form.current_result = self.current_result
+        form.game_over = self.game_over
+        form.message = message
+        return form
+
+
+class Score(ndb.Model):
+    """Score object"""
+    user = ndb.KeyProperty(required=True, kind='User')
+    date = ndb.DateProperty(required=True)
+    won = ndb.BooleanProperty(required=True)
+    score = ndb.IntegerProperty(required=True)
+
+
+# Form Models
+class GameForm(messages.Message):
+    urlsafe_key = messages.StringField(1, required=True)
+    attempts = messages.IntegerField(2, required=True)
+    game_over = messages.BooleanField(3, required=True)
+    current_result = messages.StringField(4, required=True)
+    message = messages.StringField(5, required=True)
+    user_name = messages.StringField(6, required=True)
+
+class NewGameForm(messages.Message):
+    user_name = messages.StringField(1, required=True)
+    word = messages.StringField(2, required=True)
+
+class MakeMoveForm(messages.Message):
+    guess = messages.StringField(1, required=True)
+
+class ScoreForm(messages.Message):
+    user_name = messages.StringField(1, required=True)
+    date = messages.StringField(2, required=True)
+    won = messages.BooleanField(3, required=True)
+    score = messages.IntegerField(4, required=True)
+
+
+class ScoreFroms(messages.Message):
+    items = messages.MessageField(ScoreForm, 1, repeated=True)
+
+
+class StringMessage(messages.Message):
+    message = messages.StringField(1, required=True)
+
+
